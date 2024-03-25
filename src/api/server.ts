8f23e48e7e3fe13346863fe90580a3d53e42f3e0
FelipeRibeiro <felipper43@gmail.com>
2024-03-25 20:55:44 -0300

    WIP: deciding struture

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,8 +1,9 @@
-import Express from 'express';
+import express, { Express } from 'express';
 import multer from 'multer';
 import { logger } from '../lib/logger';
+import { WhatsappClient } from '../services/whatsapp';
 
-const app = Express();
+const app = express();
 
 app.get('/', (req, res) => {
   return res.send('Hello World!');
@@ -13,3 +14,16 @@ const PORT = process.env['PORT'] || 3333;
 app.listen(PORT, () => {
   logger.info(`Api started PORT: ${PORT}`);
 });
+
+//FUNCTIONAL OR POO???
+function startAPIWithClient(client: WhatsappClient) {}
+
+class API {
+  app: Express;
+
+  constructor(PORT: number) {
+    this.app = express();
+
+    this.app.listen(PORT);
+  }
+}
