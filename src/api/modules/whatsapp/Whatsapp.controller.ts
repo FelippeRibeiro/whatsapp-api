c8ef993379b18161e9ca85fd3b0f0d99251d1586
FelippeRibeiro <felipper43@gmail.com>
2024-03-28 21:57:14 -0300

    WIP: creating route to send file - install dependencies to midias and links preview

--- src/api/modules/whatsapp/Whatsapp.controller.ts
+++ src/api/modules/whatsapp/Whatsapp.controller.ts
@@ -2,7 +2,8 @@ import { Router } from 'express';
 import { WhatsappService } from './Whatsapp.service';
 import { Whatsapp } from '../../../services/whatsapp';
 import { notConnectedHandler } from '../../middlewares/notConnected.middleware';
-
+import multer from 'multer';
+const upload = multer();
 export class WhatsappController {
   router = Router();
 
@@ -10,5 +11,6 @@ export class WhatsappController {
     this.router.use(notConnectedHandler);
     const whatsappService = new WhatsappService(whatsappClient);
     this.router.post('/whatsapp/message', whatsappService.sendMessage);
+    this.router.post('/whatsapp/file', upload.fields([{ name: 'file', maxCount: 1 }, { name: 'message' }, { name: 'to' }]), whatsappService.sendFile);
   }
 }
--- src/api/modules/whatsapp/Whatsapp.service.ts
+++ src/api/modules/whatsapp/Whatsapp.service.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { Whatsapp } from '../../../services/whatsapp';
 import { validNumber } from '../../../util/valideNumber';
+import { AnyMessageContent } from '@whiskeysockets/baileys';
 export class WhatsappService {
   whatsapp: Whatsapp;
   constructor(whatsappClient: Whatsapp) {
@@ -12,5 +13,54 @@ export class WhatsappService {
     const { number, message } = req.body;
     await this.whatsapp.client.sendMessage(`${validNumber(number)}@s.whatsapp.net`, { text: message });
     res.send({ status: 'ok' });
+  };
+
+  public sendFile = async (req: Request, res: Response) => {
+    if (req.files) {
+      try {
+        const file = (req.files as any)['file'][0] as Express.Multer.File;
+        const message = req.body.message || '';
+        const to = req.body.to || '557193277415';
+        console.log(file.mimetype);
+        const type = file.mimetype.split('/')[0];
+
+        switch (type) {
+          case 'video':
+            await this.whatsapp.client.sendMessage(`${validNumber(to)}@s.whatsapp.net`, {
+              video: file.buffer,
+              mimetype: file.mimetype,
+              caption: message,
+            });
+            break;
+          case 'audio':
+            await this.whatsapp.client.sendMessage(`${validNumber(to)}@s.whatsapp.net`, {
+              audio: file.buffer,
+              mimetype: file.mimetype,
+            });
+            break;
+
+          case 'image':
+            await this.whatsapp.client.sendMessage(`${validNumber(to)}@s.whatsapp.net`, {
+              image: file.buffer,
+              mimetype: file.mimetype,
+              caption: message,
+            });
+            break;
+
+          default:
+            await this.whatsapp.client.sendMessage(`${validNumber(to)}@s.whatsapp.net`, {
+              document: file.buffer,
+              mimetype: file.mimetype,
+              fileName: file.originalname,
+              caption: message,
+            });
+            break;
+        }
+
+        res.send({ status: 'ok' });
+      } catch (error) {
+        res.send(false);
+      }
+    }
   };
 }
