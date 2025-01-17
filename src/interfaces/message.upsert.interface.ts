0f026b88d6cf45c0b4f24585892bd224faecdc45
FelippeRibeiro <felipper43@gmail.com>
2025-01-16 22:40:20 -0300

    WIP: message controller

--- src/interfaces/message.upsert.interface.ts
+++ src/interfaces/message.upsert.interface.ts
@@ -1,6 +1,12 @@
 import { MessageUpsertType, proto } from '@whiskeysockets/baileys';
+import { Whatsapp } from '../whatsapp';
 
 export interface IMessageUpsertEventPayload {
-    messages: proto.IWebMessageInfo[];
-    type: MessageUpsertType;
+  messages: proto.IWebMessageInfo[];
+  type: MessageUpsertType;
+}
+
+export interface IMessageUpsertController {
+  instance: Whatsapp;
+  handleEvent(messagesUpsert: IMessageUpsertEventPayload): Promise<void>;
 }
